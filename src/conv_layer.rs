de73241686ac9d6350df4560de12f04db51f6844
charles-xyz <charleslazaroni@gmail.com>
2023-11-15 09:05:51 -0500

    added to conv layer

--- src/conv_layer.rs
+++ src/conv_layer.rs
@@ -58,5 +58,53 @@ impl ConvLayer {
             input: vec![],
             output: vec![vec![vec![0.0; output_size]; output_size]; num_filters],
         };
+
+        layer
+    }
+}
+
+impl Layer for ConvLayer {
+
+    // Forward propogates the input data through the Convolutional layer
+    fn forward_propagate(&mut self, input: Vec<Vec<Vec<f32>>>) -> Vec<Vec<Vec<f32>>> {
+
+        // Store the input data in a member variable for future reference
+        self.input = input.clone();
+
+        // Iterate through each output point in the output matrix
+        for y in 0..self.output_size {
+            for x in 0..self.output_size {
+                // Calculate the starting point for the convolutional kernel
+                let left = x * self.stride;
+                let top = y * self.stride;
+
+                //Iterate through each filter in the network
+                for f in 0..self.num_filters {
+                    // Initialize the output value with the bias value for the filter
+                    self.output[f][y][x] = self.biases[f];
+
+                    // Iterate through each 
+                    for f_i in 0..self.input_depth {
+                        for y_k in 0..self.kernel_size {
+                            for x_k in 0..self.kernel_size {
+                                // Get the value of the input at the current point
+                                let val: f32 = input[f_i][top + y_k][left + x_k];
+                                // Store the result of the convolution in the output matrix
+                                self.output[f][y][x] += self.kernels[f][f_i][y_k][x_k] * val;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Apply the ReLU activation function to the output
+        for i in 0..self.num_filters {
+            for y in 0..output_size {
+                for x in 0..self.output_size {
+                    self.output[f][y][x] = self.output[f][y][x].max(0.0);
+                }
+            }
+        }
     }
 }
