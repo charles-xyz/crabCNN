f6a71b6b4fde1bb3370f0edcd65efaec269f9908
charles-xyz <charleslazaroni@gmail.com>
2023-11-19 12:26:38 -0500

    finished FC layer

--- src/fully_connected_layer.rs
+++ src/fully_connected_layer.rs
@@ -70,9 +70,64 @@ fn flatten(squares: Vec<Vec<Vec<f32>>>) -> Vec<f32> {
     flat_data
 }
 
-impl layer for FullyConnectedLayer {
+impl Layer for FullyConnectedLayer {
     // Calculate the output layer by forward propagating the input layer
     fn forward_propagate(&mut self, matrix_input: Vec<Vec<Vec<f32>>>) -> Vec<Vec<Vec<f32>>> {
+        let input: Vec<f32> = flatten(matrix_input);
+        // Store the input for backpropagation
+        self.input = input.clone();
         
+        for j in 0..self.output_size {
+            //Calculate the weighted sum of the inputs
+            self.output[j] = self.biases[j];
+            for i in 0..self.input_size {
+                self.output[j] += input[i] * self.weights[i][j];
+            }
+            // Apply the sigmoid activation function to the output
+            self.output[j] = sigmoid(self.output[j]);
+        }
+
+        // Format the output to be a 3D vector
+        let formatted_output: Vec<Vec<Vec<f32>>> = vec![vec![self.output.clone()]];
+        formatted_output
+    }
+
+    // Backpropagate the gradient up the FC layer
+    // Update the weights and biases of the current layer
+    // Return the error of the previous layer
+    fn back_propagate(&mut self, matrix_error: Vec<Vec<Vec<f32>>>) -> Vec<Vec<Vec<f32>>> {
+        // Flatten the error matrix into a 1d vector
+        let mut error: Vec<f32> = matrix_error[0][0].clone();
+        for j in 0..self.output_size {
+            error[j] *= inv_deriv_sigmoid(self.output[j]);
+        }
+
+        let mut flat_error: Vec<f32> = vec![0.0; self.input_size];
+
+        // Update the weights and biases according to their derivatives
+        for j in 0..self.output_size {
+            self.biases[j] -= error[j] * LEARNING_RATE;
+            for i in 0..self.input_size {
+                flat_error[i] += error[j] * self.weights[i][j];
+                self.weights[i][j] -= error[j] * self.input[i] * LEARNING_RATE;
+            }
+        }
+
+        // Format the error to be a 3D vector
+        let mut prev_error: Vec<Vec<Vec<f32>>> = vec![vec![vec![]; self.input_width]; self.input_depth];
+        for i in 0..self.input_depth {
+            for j in 0..self.input_width{
+                for k in 0..self.input_width {
+                    let index: usize = i * self.input_width.pow(2) + j * self.input_width + k;
+                    prev_error[i][j].push(flat_error[index]);
+                }
+            }
+        }
+
+        prev_error
+    }
+
+    fn get_output(&mut self, index: usize) -> f32 {
+        self.output[index]
     }
 }
